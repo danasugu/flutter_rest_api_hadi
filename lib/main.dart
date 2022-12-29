a275141987be694c2aaa133b56b13b19bb5d0402
Dana Sugu <dana.sugu@gmail.com>
2022-12-29 15:17:41 +0200

    force orietation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  //force orietation to portrait
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
